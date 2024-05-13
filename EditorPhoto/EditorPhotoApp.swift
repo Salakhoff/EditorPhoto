66cdfebc6ee9303981534f07f60161e05734dec5
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-13 11:20:50 +0300

    Initial Commit

---
+++ EditorPhoto/EditorPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorPhotoApp.swift
+//  EditorPhoto
+//
+//  Created by Ильфат Салахов on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct EditorPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
